13829404d71735d20ac6d0815d6f2147bba692f3
red2231 <erickverissimo57@gmail.com>
2025-03-16 16:39:23 -0300

    changes final

--- src/teste.php
+++ src/teste.php
@@ -7,7 +7,7 @@ use Predis\Client;
 require_once __DIR__.'/../vendor/autoload.php';
 $dotenv = Dotenv::createMutable(__DIR__ . '/../');
 $dotenv->safeLoad();
-$host = $_ENV['HOST'];
+$host = 'redis.railway.internal';
 $username = $_ENV['USERNAME'];
 $password = $_ENV['PASSWO'];
 function exists(string $userId): bool {
