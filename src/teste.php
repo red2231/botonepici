58b39b914d4ba4999f1045d663ea2b17be9458d9
red2231 <erickverissimo57@gmail.com>
2025-03-16 16:15:20 -0300

    oq

--- src/teste.php
+++ src/teste.php
@@ -1,20 +1,23 @@
 <?php
 namespace Discord\Proibida;
 use Carbon\Carbon;
+use Dotenv\Dotenv;
 use Predis\Client;
 
 require_once __DIR__.'/../vendor/autoload.php';
-
+$dotenv = Dotenv::createMutable(__DIR__ . '/../');
+$dotenv->load();
+$host = $_ENV['host'];
 
 function exists(string $userId):bool|string{
 return (new Client([
-    'host' => 'localhost',
+    'host' => $GLOBALS['host'],
     'port' => 6379
 ]))->exists($userId);
 }
 function check(string $userId){
     $client = new Client([
-        'host' => 'localhost',
+        'host' => $GLOBALS['host'],
         'port' => 6379
     ]);
 
