0bb8a0f928ee40fb5b8bd81b831aceb197d00f46
red2231 <erickverissimo57@gmail.com>
2025-03-19 19:43:38 -0300

    migracao

---
+++ src/Entities/AkumaToAdd.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Discord\Proibida\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class AkumaToAdd
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+    #[ORM\Column]
+    private string $name;
+    #[ORM\Column]
+    private ?string $description;
+
+
+
+    /**
+     * Get the value of description
+     *
+     * @return ?string
+     */
+    public function getDescription(): ?string {
+        return $this->description;
+    }
+
+    /**
+     * Set the value of description
+     *
+     * @param ?string $description
+     *
+     * @return self
+     */
+    public function setDescription(?string $description): self {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self {
+        $this->id = $id;
+        return $this;
+    }
+}
---
+++ src/migração.php
@@ -0,0 +1,29 @@
+<?php
+
+use Discord\Proibida\Entities\Akuma;
+use Discord\Proibida\Entities\AkumaToAdd;
+use Discord\Proibida\Entities\Usuario;
+
+use function Discord\Proibida\getEntityManager;
+
+function frontHandler(string $akumaName,string $userId, string $url ){
+$EntityManager = getEntityManager();
+$repo = $EntityManager->getRepository(Akuma::class);
+$query= $repo->createQueryBuilder('u')
+->where(['u.name =:name'])
+->setParameter('name', $akumaName);
+$akuma =$query->getQuery()->getOneOrNullResult();
+if(!$akuma){
+$toAdd = new AkumaToAdd;
+$toAdd->setName($akumaName);
+$EntityManager->persist($toAdd);
+$EntityManager->flush();
+return false;
+}else{
+$user = new Usuario($userId);
+$user->setAvatarUrl($url);
+$user->setAkuma($akuma);
+$EntityManager->persist($user);
+$EntityManager->flush();
+return true;
+}}
